599b888a9e3ceeacb78221b6db0ca57c2ee7d31a
everest275 <eaguilarc112@uacit.ed.cr>
2023-04-04 15:36:04 -0600

    addsecond

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://everest275.github.io',
-  base: '/airportshuttle-landing',
+  integrations: [tailwind(), react()]
 });
